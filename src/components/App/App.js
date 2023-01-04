09ac4bd0273913d83ed7154a2af6fe5c031c2d4f
Anthony Shellman <atshellman@gmail.com>
2023-01-04 15:04:33 -0700

    Add import, useEffect and state

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,18 @@
 import React, {useState, useEffect} from 'react'
+import {getArticles} from "../../utilities/apiCalls"
 import './App.css';
 
 const App = () => {
+  const [articles, setArticles] = useState([]) // pass as prop
+  const [errorMsg, setErrorMsg] = useState('') // render error somewhere
+
+  useEffect(() => {
+    getArticles('science', setErrorMsg)
+    .then(formattedData => setArticles(formattedData))
+  },[])
+
   return (
-    <main className="App">
+    <main className="app">
    
     </main>
   );
