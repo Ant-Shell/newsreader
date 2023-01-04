13f98a067a3cc8406bcc45761164bb0c346f89e4
Anthony Shellman <atshellman@gmail.com>
2023-01-04 15:19:07 -0700

    Initial commit

---
+++ src/components/ArticleDetails/ArticleDetails.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./ArticleDetails.css"
+
+const ArticleDetails = () => {
+  return (
+    <div className="App">
+   
+    </div>
+  );
+}
+
+export default ArticleDetails;
