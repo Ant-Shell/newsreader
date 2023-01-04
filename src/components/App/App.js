add05c201a0504c941e19247bf72777a8e790784
Anthony Shellman <atshellman@gmail.com>
2023-01-03 18:21:13 -0700

    Initial setup

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import React, {useState, useEffect} from 'react'
+import './App.css';
+
+const App = () => {
+  return (
+    <main className="App">
+   
+    </main>
+  );
+}
+
+export default App;
