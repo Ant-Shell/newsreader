663d85d3142cbcb1e41df93addb7026232f464bc
Anthony Shellman <atshellman@gmail.com>
2023-01-04 20:47:39 -0700

    Attempt to render images as card backgrounds

--- src/components/ArticleCard/ArticleCard.js
+++ src/components/ArticleCard/ArticleCard.js
@@ -6,8 +6,9 @@ const ArticleCard = ({ id, section, subsection, title, multimedia}) => {
   return (
     <div className='card'>
       <p>{title}</p>
-      <p>{section}</p>
-      <p>{subsection}</p>
+      <p>Section: {section}</p>
+      {subsection ? <p>Topic: {subsection}</p> : null}
+      <img className="image" src={multimedia[1].url} alt={multimedia[1].caption}></img>
     </div>
   );
 }
--- src/components/Articles/Articles.js
+++ src/components/Articles/Articles.js
@@ -4,14 +4,14 @@ import "./Articles.css"
 
 const Articles = ({ articles }) => {
   const articleList = articles.map(article => {
-    const {id, section, subsection, title, mutimedia} = article
+    const {id, section, subsection, title, multimedia} = article
     return (
       <ArticleCard 
         id={id}
         section={section}
         subsection={subsection}
         title={title}
-        mulitmedia={mutimedia}
+        multimedia={multimedia}
         key={id}
       />
     )
