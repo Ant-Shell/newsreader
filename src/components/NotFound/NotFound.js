7280d2988e683c9e96666dd406f164b8d5186805
Anthony Shellman <atshellman@gmail.com>
2023-01-05 08:02:39 -0700

    Add page not found comonent

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./NotFound.css"
+
+const NotFound = () => {
+  return (
+    <div>
+      <h2>
+        {`Sorry, this page was not found\nPlease try again.`}
+      </h2>
+    </div>
+  )
+}
+
+export default NotFound
