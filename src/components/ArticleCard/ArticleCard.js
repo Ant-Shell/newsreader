d4c922c686af70f1e5c0de1c5c8991e71cb7e752
Anthony Shellman <atshellman@gmail.com>
2023-01-06 13:41:15 -0700

    Add images to cards

--- src/components/ArticleCard/ArticleCard.js
+++ src/components/ArticleCard/ArticleCard.js
@@ -4,12 +4,11 @@ import "./ArticleCard.css"
 
 const ArticleCard = ({ id, title, multimedia}) => {
   return (
-    <Link to={`${id}`}>
     <section className='card' data-cy="card">
-      <p>{title}</p>
-      {/* <img className="image" src={multimedia[1].url} alt={multimedia[1].caption} /> */} {/* Should be background - fix this later */}
+    <Link to={`${id}`}><img className="image" src={multimedia[1].url} alt={multimedia[1].caption}></img></Link>
+        <p>{title}</p>
     </section>
-    </Link>
+    
   );
 }
 
